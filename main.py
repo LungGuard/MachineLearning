544758dca8f032da277adc3591f8aff9f0e6611e
Ilay Nagar <ilay8073@gmail.com>
2026-04-11 16:04:47 +0300

    feat(api): Add /api/models/analyze endpoint with DICOM upload

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
 from common.dto import PipelineResults
 from common.constants import ApiConstants
 from contextlib import asynccontextmanager
 from middleware.gateway_auth import GatewayAuthMiddleware
 from middleware.eureka_config import register_eureka, deregister_eureka
 from middleware.middleware_config import EurekaConfig
+from typing import Annotated
 
-config = EurekaConfig()
+
+eureka_config = EurekaConfig()
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
-    register_eureka(config=config)
+    register_eureka(config=eureka_config)
     yield
     deregister_eureka()
 
@@ -23,8 +25,10 @@ app = FastAPI(
 app.add_middleware(GatewayAuthMiddleware)
 
 
-
-@app.post("/api/analyze",response_model=PipelineResults)
-def analyze():
+@app.post("/api/models/analyze",response_model=PipelineResults)
+def analyze(dicom_files: Annotated[
+        list[UploadFile],
+        File(description="CT scan DICOM series files (.dcm)"),
+    ]):
     pass
 
